5990b3edee743bcabe33e5ea26b06d7a8299f6c3
Julieta <marcerjulieta@gmail.com>
2022-10-18 21:15:36 -0300

    sigue sin andar el metodo PUT, falta middleware propio

--- src/services/modificarUsuarioService.js
+++ src/services/modificarUsuarioService.js
@@ -1,15 +1,12 @@
 const UsuarioModel = require("../models/usuarios.model");
 
 const modifcarUsuarioService = async (req) => {
-    console.log(req.params);
     const usuario = req.body;
-    console.log(usuario);
-    const  _id  = req.params._id
+    const  {_id}  = req.params;
     console.log(_id);
-    
-    const updateUsuario = await UsuarioModel.findById(_id);
+    const updateUsuario = await UsuarioModel.findById(_id).exec();
     console.log(updateUsuario);
-
+    if(!updateUsuario)throw new Error("Usuario not found");
     updateUsuario.name = usuario.name;
     updateUsuario.age = usuario.age;
     updateUsuario.sex = usuario.sex;
